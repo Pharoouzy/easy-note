af65bef623854a346b694f6c89522ca161816f58
Pharoouzy <pharoouzy@yahoo.com>
2020-04-17 18:11:19 +0100

    Complete endpoint for adding a note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -2,7 +2,28 @@ const Note = require("../models/note.model.js");
 
 // Create and Save a new Note
 exports.create = (req, res) => {
-    
+    // Validate incoming request
+    if(!req.body.content){
+        return res.status(400).send({
+            message: "Note content cannot be blank"
+        });
+    }
+
+
+    // create new note
+    const note = new Note({
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    });
+
+    // save note in database
+    note.save().then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
 };
 
 // Retrieve and return all notes from the database.
