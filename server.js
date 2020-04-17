7640637e861ad4e29f73ecc5f0f23896e3912f91
Pharoouzy <pharoouzy@yahoo.com>
2020-04-17 17:45:19 +0100

    Database configuration and connection
    Configured and Connected the app to DB and Created a Note Model

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+// Configuring the database
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(() => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+
 //define a simple route
 app.get("/", (req, res) => {
     res.json({
