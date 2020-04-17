c1c86ef3f6d8cff331701d89742d0182b53a0271
Pharoouzy <pharoouzy@yahoo.com>
2020-04-17 18:28:07 +0100

    Complete endpoints for retrieving all note and a single note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -17,8 +17,8 @@ exports.create = (req, res) => {
     });
 
     // save note in database
-    note.save().then(data => {
-        res.send(data);
+    note.save().then(notes => {
+        res.status(201).send(notes);
     }).catch(err => {
         res.status(500).send({
             message: err.message || "Some error occurred while creating the Note."
@@ -28,13 +28,35 @@ exports.create = (req, res) => {
 
 // Retrieve and return all notes from the database.
 exports.getAll = (req, res) => {
-
+    Note.find().then((notes) => {
+        res.send(notes);
+    }).catch((err) => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving notes."
+        });
+    });
 };
 
 
 // Find a single note with a noteId
 exports.get = (req, res) => {
-
+    Note.findById(req.params.noteId).then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: `Note not found with id ${req.params.noteId}`
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: `Note not found with id ${req.params.noteId}`
+            });
+        }
+        res.status(500).send({
+            message: `Error retrieving note with id ${req.params.noteId}`
+        })
+    });
 };
 
 // Update a note identified by the noteId in the request
